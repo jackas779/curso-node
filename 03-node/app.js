a641f9b9b491f90c28f8b938e049d0f46755adf3
Nicolas Ardila <jackas779@gmail.com>
2024-07-21 22:29:14 -0500

    clase numero 3 de node.js configuracion de rutas y esquemas

---
+++ 03-node/app.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const crypto = require('crypto')
+const cors = require('cors')
+const api = express()
+const movieJSON = require('./movies.json')
+const { validateMovie, partialMovie } = require('./schemas/movies')
+
+const PORT = process.env.PORT ?? 1234
+
+api.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://127.0.0.1:5500',
+      'http://localhost:8080',
+      'https://movies.com',
+      'https://midu.dev'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+
+api.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+api.use(express.json())
+
+api.get('/movies', (req, res) => {
+  const { genero } = req.query
+
+  if (genero) {
+    const filtersMovies = movieJSON.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genero.toLowerCase())
+    )
+    return res.json(filtersMovies)
+  }
+
+  res.json(movieJSON)
+})
+
+api.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: result.error.issues[0].message })
+  }
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
+  movieJSON.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+api.patch('/movies/:id', (req, res) => {
+  const result = partialMovie(req.body)
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+  const { id } = req.params
+  const movieIndex = movieJSON.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(400).json({ message: 'Pelicula no encontrada ' })
+  }
+  const udapteMovie = {
+    ...movieJSON[movieIndex],
+    ...result.data
+  }
+
+  res.json(udapteMovie)
+})
+
+api.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movieJSON.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(400).json({ message: 'Pelicula no encontrada ' })
+  }
+
+  movieJSON.slice(movieIndex, 1)
+  res.json({ message: 'Pelicula eliminada' })
+})
+
+api.use((req, res) => {
+  res.status(400).send('<h1> 404 NOT FOUND </h1>')
+})
+
+api.listen(PORT, () => {
+  console.log(`Servidor escuchando desde http://localhost:${PORT}`)
+})
