15626cd7eb1d5708fca7decce089c7aecdc6dd45
Nicolas Ardila <jackas779@gmail.com>
2025-02-17 13:53:39 -0500

    esquema de creacion de usuariio en swagger creado y corregido

--- practice/schemas/user.mjs
+++ practice/schemas/user.mjs
@@ -5,13 +5,13 @@ import { z } from 'zod'
 
 const userSchema = z.object({
   username: z.string().min(7).max(30),
-  password: z.string().min(8).max(30),
+  firstName: z.string().min(3).max(30),
+  lastName: z.string().min(3).max(30),
   email: z.string().email(),
+  password: z.string().min(8).max(30),
+  phone: z.string().min(10).max(10).length(10),
   age: z.number().int().positive().min(16),
   isAdult: z.boolean(),
-  phone: z.string().min(10).max(10).length(10),
-  firtsName: z.string().min(3).max(30),
-  lastName: z.string().min(3).max(30)
 })
 
 export const validateUser = (input) => userSchema.safeParse(input)
